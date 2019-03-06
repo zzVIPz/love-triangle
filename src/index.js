54e509946cd52d412dabe767d5454905334c69f2
zzVIPz <viperexe@mail.ru>
2019-03-07 00:04:22 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let heroOneValue,
+    heroTwoValue,
+    count = 0;
+
+  for (let i = 0; i <= preferences.length - 1; i++) {
+    heroOneValue = preferences[i];
+    heroTwoValue = preferences[heroOneValue - 1];
+    if (heroOneValue === heroTwoValue) continue;
+    if (preferences[heroTwoValue - 1] === i + 1) {
+      count++;
+    }
+  }
+  return count / 3;
 };
